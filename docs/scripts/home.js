d0b158d8d7f686d840a878f7b45b54598c5c6762
Jackson Tian <shyvo1987@gmail.com>
2011-09-12 01:11:41 +0800

    Comment the scripts.

--- docs/scripts/home.js
+++ docs/scripts/home.js
@@ -4,18 +4,6 @@ V5.registerPage("home", function () {
     var initialize = function () {
         var page = this;
         var view = V5.View(page.node);
-        var proxy = new EventProxy();
-        proxy.assign("template", "data", function (template, data) {
-            var html = _.template(template, data);
-            view.$("#listing").html(html);
-        });
-        V5.getTemplate("listview", function (template) {
-            proxy.trigger("template", template);
-        });
-
-        $.get("ajax/listview.json", function (data) {
-            proxy.trigger("data", JSON.parse(data));
-        });
 
         view.bind("getDetail", function (event) {
             var target = $(event.currentTarget);
@@ -24,7 +12,7 @@ V5.registerPage("home", function () {
         });
 
         view.delegateEvents({
-            "click #listing li": "getDetail",
+            //"click #listing li": "getDetail",
         });
     };
 
