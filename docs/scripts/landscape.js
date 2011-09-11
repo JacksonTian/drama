dc8f199c717e2b5ee0534f2315ec8a50b21aa669
Jackson Tian <shyvo1987@gmail.com>
2011-09-12 02:07:26 +0800

    Add landscape

---
+++ docs/scripts/landscape.js
@@ -0,0 +1,12 @@
+V5.registerPage("landscape", function () {
+    console.log("Landscape page");
+
+    var initialize = function () {
+
+    };
+
+    return {
+        initialize : initialize
+    };
+
+});
